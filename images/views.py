5152862c8176023fa106e5d8e17873561aa6c9b8
LuisBell0 <lalfon11@gmail.com>
2025-04-20 15:53:55 -0400

    Created permissions for users to only perform actions with their images

--- images/views.py
+++ images/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .permissions import IsOwner
 from .serializers import ImageSerializer
 from .models import ImageConversion
 
@@ -7,4 +10,9 @@ from .models import ImageConversion
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = ImageConversion.objects.all()
     serializer_class = ImageSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def list(self, request, *args, **kwargs):
+        queryset = ImageConversion.objects.filter(user=request.user)
+        serializer = ImageSerializer(queryset, many=True)
+        return Response(serializer.data)
